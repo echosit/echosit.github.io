0aff8e72e056c7e07392136a0e57bb508e0f2dde
Echo Sit <echosit@Echos-MacBook-Air.local>
2021-01-15 13:44:35 -0800

    Updates

--- precache-manifest.69825bd8bb7fc015f6aaf22137a78bc1.js
+++ precache-manifest.69825bd8bb7fc015f6aaf22137a78bc1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "042d0de8a14c1950959f400774f6dfe8",
+    "revision": "b89f805e7968cd546a53ad6324122a16",
     "url": "/index.html"
   },
   {
-    "revision": "51453fabf4af1700232d",
-    "url": "/static/js/2.27e142ce.chunk.js"
+    "revision": "0c711083af123a9cf853",
+    "url": "/static/js/2.0ac0770f.chunk.js"
   },
   {
     "revision": "6472a7d1d95598048b2f7b02095da167",
-    "url": "/static/js/2.27e142ce.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.0ac0770f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cf4a39589ae8f9499b1a",
-    "url": "/static/js/main.a1abfbf6.chunk.js"
+    "revision": "367f9b2a3c7e44ede0b8",
+    "url": "/static/js/main.230db86f.chunk.js"
   },
   {
     "revision": "283e764f5f4687959e63",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f22f04093aaccecc9938441edd01be3c.js"
+  "/precache-manifest.69825bd8bb7fc015f6aaf22137a78bc1.js"
 );
 
 self.addEventListener('message', (event) => {
